569bcd8a19cb25ff4b586988a1b6a68c8dd6ceab
RyanTheAllmighty <ryan@ryandowling.me>
2015-10-22 23:49:33 +1100

    Give the alert box more room for large inputs

--- assets/static/js/alerts.js
+++ assets/static/js/alerts.js
@@ -48,6 +48,6 @@
     function showAlert(time) {
         var $alert = $('.alert');
 
-        $alert.transition({x: '170px'}).transition({opacity: 0, delay: time}, 500, 'linear').transition({x: '-165px'}).transition({opacity: 100});
+        $alert.transition({x: '505px'}).transition({opacity: 0, delay: time}, 500, 'linear').transition({x: '-505px'}).transition({opacity: 100});
     }
 })();
