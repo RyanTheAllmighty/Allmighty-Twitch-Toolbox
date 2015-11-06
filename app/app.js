eac2f6b481d2e5f533fae7def373fb5dbd40e172
RyanTheAllmighty <ryan@ryandowling.me>
2015-11-07 07:46:05 +1100

    Move notifications to the bottom right so as to not obstruct the menu

--- app/app.js
+++ app/app.js
@@ -115,12 +115,12 @@
         // Setup the NotificationProvider
         NotificationProvider.setOptions({
             delay: 10000,
-            startTop: 20,
+            startBottom: 20,
             startRight: 10,
             verticalSpacing: 20,
             horizontalSpacing: 20,
             positionX: 'right',
-            positionY: 'top'
+            positionY: 'bottom'
         });
     });
 
